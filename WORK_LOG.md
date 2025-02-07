5832e791f212d2de3d9cd8249213cb34d703ccf0
Josh Little <josh.little@menloschool.org>
2025-02-07 09:50:25 -0700

    More comments. Can't add more bc leaving for comp.

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
-Name: YOUR NAME HERE
+Name: Josh Litlte
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date       |     Time     |                                                                                                                                              Update |
+|:-----------|:------------:|----------------------------------------------------------------------------------------------------------------------------------------------------:|
+| April 5    |    9-10pm    |                                                                                                          I tried to write abstract, efficient code. |
+| February 4 | 12:35-1:45pm |                                                      Worked in class to complete memoization and spent the rest of the time attempting tabulation.  |
+| February 5 | 6:00-6:10pm  | Continued working on tabulation but wasn't able to complete it and was in the middle of moving + packing for robotics trip so didn't have any time. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MakingChange.java
+++ src/MakingChange.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 /**
  * The class Making Change solves a classic problem:
  * given a set of coins, how many ways can you make change for a target amount?
  *
  * @author Zach Blick
- * @author [YOUR NAME HERE]
+ * @author Josh Little
  */
 
 public class MakingChange {
@@ -12,6 +14,30 @@ public class MakingChange {
      *  for any given total with any given set of coins.
      */
     public static long countWays(int target, int[] coins) {
-        return 0;
+        // Sorts the array of coins from least to greatest
+        Arrays.sort(coins);
+        // Creates a map to store the number of coins required for combinations of target
+        // length and using only a subset of coins.
+        long[][] map = new long[target + 1][coins.length];
+        // Go through all the combinations that can make 0 cents and set them equal to 1.
+        for (int i = 0; i < coins.length; i++)
+            map[0][i] = 1;
+        // Memoization
+        return countWays(target, map, coins, coins.length - 1);
     }
+
+    public static long countWays(int target, long[][] map, int[] coins, int index) {
+        if (index < 0)
+            return 0;
+        if (target < 0)
+            return 0;
+        // If there's a value stored for a specific combination, return it.
+        if (map[target][index] != 0)
+            return map[target][index];
+        // Set the value of a target + subset of coins pair using index as max by using recursion
+        map[target][index] = countWays(target - coins[index], map, coins, index) + countWays(target, map, coins, index - 1);
+        // return the value above for the recursive call.
+        return map[target][index];
+    }
+
 }
